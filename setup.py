cd8f0034fc99bc182153c8539682e324e52fc537
Alex Merk <durango77@gmail.com>
2021-12-06 00:35:25 +0300

    теперь ок

---
+++ setup.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from weather import weather_by_city
+app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello():
+#   return "Привет!"
+
+
+@app.route("/")
+def index():
+    weather = weather_by_city("Moscow,Russia")
+    return f"Сейчас {weather['temp_C']},ощущается как {weather['FeelsLikeC']}"
+
+
+if __name__ == "__main__":
+    app.run()
